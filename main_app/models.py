66bfb24e1958a2a5cc4d69d36f777252f4f3aee4
nonchalamment <nonchalamment@gmail.com>
2023-05-30 14:26:18 -0700

    add new images and create first model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Yarn(models.Model):
+  name = models.CharField(max_length=100)
+  weight = models.CharField(max_length=50)
+  fiber = models.CharField(max_length=100)
+  description = models.CharField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+class Yarn:
+  def __init__(self, name, weight, fiber, description):
+    self.name = name
+    self.weight = weight
+    self.fiber = fiber
+    self.description = description
+
+yarns = [
+  Yarn('Madelinetosh Tosh Vintage', 'Worsted', '100% Merino Wool', 'Machine wash cold. Air dry flat.'),
+  Yarn('Malabrigo Rasta', 'Super Bulky', '100% Merino Wool', 'Hand wash. Dry flat.'),
+  Yarn('Malabrigo Silky Merino', 'DK', '51% Silk, 49% Merino Wool', 'Hand wash. Dry flat.'),
+  Yarn('Handmaiden Sea Silk', 'Fingering', '70% Silk, 30% Seacell', 'Hand wash.')
+]
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>I am Yarn Collector!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def yarn_index(request):
+  return render(request, 'yarns/index.html', { 'yarns': yarns })
